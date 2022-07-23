3de79724f2cc196684d5d2f94f3568be9149f1b8
Risabh Rai <risabhrai2001@gmail.com>
2022-07-23 10:05:48 +0530

    minor fix #4

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 "use strict";
 var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function (t) {
+    __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
             s = arguments[i];
             for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
@@ -16,18 +16,18 @@ var cors_1 = require("cors");
 var mongoose_1 = require("mongoose");
 var user_1 = require("./model/user");
 var jsonwebtoken_1 = require("jsonwebtoken");
-var app = express_1();
+var app = (0, express_1["default"])();
 var url = process.env.URL;
 var token_key = process.env.TOKEN_KEY;
 mongoose_1["default"]
     .connect(url)
     .then(function () {
-        console.log("Connected to the database ");
-    })["catch"](function (err) {
-        console.error("Error connecting to the database. ".concat(err));
-    });
-app.use((cors_1()));
-app.use(express_1.json());
+    console.log("Connected to the database ");
+})["catch"](function (err) {
+    console.error("Error connecting to the database. ".concat(err));
+});
+app.use((0, cors_1["default"])());
+app.use(express_1["default"].json());
 if (process.env.NODE_ENV == "production") {
     app.use(express_1["default"].static("client/build"));
     var path_1 = require("path");
