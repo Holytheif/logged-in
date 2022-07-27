6c742d22962b8a7d4d20c417867f6a13103e7e4d
Risabh Rai <risabhrai2001@gmail.com>
2022-07-27 14:39:57 +0530

    minor fix #4

--- index.ts
+++ index.ts
@@ -31,13 +31,9 @@ mongoose
 app.use(CORS());
 app.use(express.json());
 if (process.env.NODE_ENV == "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static("public"));
 
 	const path = require("path");
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "public", "index.html"));
-	});
 }
 app.post("/signup", (req: Request, res: Response) => {
 	const signUpFormData: FormDataType = req.body;
